c792e07fee592c812995c8598d00d30708846658
Alymova-crypto <alymova.a@gmail.com>
2022-07-20 13:42:34 +0300

    added a search engine

---  vanilla-weather-app/src/app.js
+++  vanilla-weather-app/src/app.js
@@ -43,8 +43,16 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let city = "Barcelona";
-let apiKey = "8020162217ba2a2e835a4a632f881993";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "8020162217ba2a2e835a4a632f881993";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("New York");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
